03b33d04cf35d245d1e7f7b13ce30ae41053ba48
Sachin kumar <skumar@mail.bss401dt012>
2016-11-02 15:02:30 +0530

    initial commit

--- config/app.default.php
+++ config/app.default.php
@@ -217,7 +217,7 @@ return [
             //'port' => 'nonstandard_port_number',
             'username' => 'my_app',
             'password' => 'secret',
-            'database' => 'my_app1',
+            'database' => 'my_app',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'cacheMetadata' => true,
